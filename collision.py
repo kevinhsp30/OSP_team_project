8a11eb3d07acb526214e531a6d520fd84c6fcbaf
hyunsu Park <kevinhsp@naver.com>
2022-12-06 07:09:10 +0900

    충돌함수

--- collision.py
+++ collision.py
@@ -1,10 +1,13 @@
 import setup
 
 # 충돌함수 구현
-print(setup.obs_list[0].posX)
+# print(setup.obs_list[0].posX)
 
 
 is_Collision = False
+
 def check_collision(count):
-    if (setup.obs_list[count].posX) < setup.Puang.posX < (setup.obs_list[count].posX + setup.obs_list[count].width) and 
+    if setup.Puang.posX <= (setup.obs_list[count].posX + setup.obs_list[count].width) and setup.Puang.posX + setup.Puang.width >= setup.obs_list[count].posX:
+        if setup.Puang.posY + setup.Puang.height >= setup.obs_list[count].height:
+            is_Collision = True
     
